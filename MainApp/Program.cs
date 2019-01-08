73cd2de765ed102756057cbf0a5cd49baa0ca66e
Alex Serov <alex.serov.mail@gmail.com>
2019-01-08 19:42:39 +0300

    Init commit

---
+++ MainApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MainApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main app");
+            Console.ReadKey();
+        }
+    }
+}
